9a678d0a78826daf1a05504ad058067de0259207
hdcmeta <hdcmeta@gmail.com>
2015-12-21 23:47:28 -0800

    Remove code for full-screen exclusive

--- Source/Core/VideoBackends/D3D12/Render.cpp
+++ Source/Core/VideoBackends/D3D12/Render.cpp
@@ -1099,35 +1099,10 @@ void Renderer::SwapImpl(u32 xfbAddr, u32 fbWidth, u32 fbStride, u32 fbHeight, co
 	// Flip/present backbuffer to frontbuffer here
 	D3D::Present();
 
-	// Check exclusive fullscreen state
-	bool exclusive_mode, fullscreen_changed = false;
-	if (SUCCEEDED(D3D::GetFullscreenState(&exclusive_mode)))
-	{
-		if (fullscreen && !exclusive_mode)
-		{
-			if (g_Config.bExclusiveMode)
-				OSD::AddMessage("Lost exclusive fullscreen.");
-
-			// Exclusive fullscreen is enabled in the configuration, but we're
-			// not in exclusive mode. Either exclusive fullscreen was turned on
-			// or the render frame lost focus. When the render frame is in focus
-			// we can apply exclusive mode.
-			fullscreen_changed = Host_RendererHasFocus();
-
-			g_Config.bExclusiveMode = false;
-		}
-		else if (!fullscreen && exclusive_mode)
-		{
-			// Exclusive fullscreen is disabled, but we're still in exclusive mode.
-			fullscreen_changed = true;
-		}
-	}
-
 	// Resize the back buffers NOW to avoid flickering
 	if (CalculateTargetSize(s_backbuffer_width, s_backbuffer_height) ||
 		xfbchanged ||
 		windowResized ||
-		fullscreen_changed ||
 		s_last_efb_scale != g_ActiveConfig.iEFBScale ||
 		s_last_multisamples != g_ActiveConfig.iMultisamples ||
 		s_last_stereo_mode != (g_ActiveConfig.iStereoMode > 0))
@@ -1136,23 +1111,8 @@ void Renderer::SwapImpl(u32 xfbAddr, u32 fbWidth, u32 fbStride, u32 fbHeight, co
 		s_last_multisamples = g_ActiveConfig.iMultisamples;
 		PixelShaderCache::InvalidateMSAAShaders();
 
-		if (windowResized || fullscreen_changed)
+		if (windowResized)
 		{
-			// Apply fullscreen state
-			if (fullscreen_changed)
-			{
-				g_Config.bExclusiveMode = fullscreen;
-
-				if (fullscreen)
-					OSD::AddMessage("Entered exclusive fullscreen.");
-
-				D3D::SetFullscreenState(fullscreen);
-
-				// If fullscreen is disabled we can safely notify the UI to exit fullscreen.
-				if (!g_ActiveConfig.bFullscreen)
-					Host_RequestFullscreen(false);
-			}
-
 			// TODO: Aren't we still holding a reference to the back buffer right now?
 			D3D::Reset();
 
