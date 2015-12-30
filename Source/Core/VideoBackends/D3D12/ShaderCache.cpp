d2a80594a64d1c75328414e01dc1eed0d4382247
hdcmeta <hdcmeta@gmail.com>
2015-12-29 18:14:32 -0800

    Update ShaderCache in response to CR feedback

--- Source/Core/VideoBackends/D3D12/ShaderCache.cpp
+++ Source/Core/VideoBackends/D3D12/ShaderCache.cpp
@@ -18,40 +18,40 @@ namespace DX12
 
 // Primitive topology type is always triangle, unless the GS stage is used. This is consumed
 // by the PSO created in Renderer::ApplyState.
-D3D12_PRIMITIVE_TOPOLOGY_TYPE current_primitive_topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+static D3D12_PRIMITIVE_TOPOLOGY_TYPE s_current_primitive_topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 
-typedef std::map<GeometryShaderUid, D3D12_SHADER_BYTECODE> GsBytecodeCache;
-typedef std::map<PixelShaderUid, D3D12_SHADER_BYTECODE> PsBytecodeCache;
-typedef std::map<VertexShaderUid, D3D12_SHADER_BYTECODE> VsBytecodeCache;
-GsBytecodeCache gs_bytecode_cache;
-PsBytecodeCache ps_bytecode_cache;
-VsBytecodeCache vs_bytecode_cache;
+using GsBytecodeCache = std::map<GeometryShaderUid, D3D12_SHADER_BYTECODE>;
+using PsBytecodeCache = std::map<PixelShaderUid, D3D12_SHADER_BYTECODE>;
+using VsBytecodeCache = std::map<VertexShaderUid, D3D12_SHADER_BYTECODE>;
+static GsBytecodeCache s_gs_bytecode_cache;
+static PsBytecodeCache s_ps_bytecode_cache;
+static VsBytecodeCache s_vs_bytecode_cache;
 
 // Only used for shader debugging..
-typedef std::map<GeometryShaderUid, std::string> GsHlslCache;
-typedef std::map<PixelShaderUid, std::string> PsHlslCache;
-typedef std::map<VertexShaderUid, std::string> VsHlslCache;
-GsHlslCache gs_hlsl_cache;
-PsHlslCache ps_hlsl_cache;
-VsHlslCache vs_hlsl_cache;
+using GsHlslCache = std::map<GeometryShaderUid, std::string>;
+using PsHlslCache = std::map<PixelShaderUid, std::string>;
+using VsHlslCache = std::map<VertexShaderUid, std::string>;
+static GsHlslCache s_gs_hlsl_cache;
+static PsHlslCache s_ps_hlsl_cache;
+static VsHlslCache s_vs_hlsl_cache;
 
-LinearDiskCache<ShaderGeneratorInterface, u8> g_gs_disk_cache;
-LinearDiskCache<ShaderGeneratorInterface, u8> g_ps_disk_cache;
-LinearDiskCache<ShaderGeneratorInterface, u8> g_vs_disk_cache;
+static LinearDiskCache<ShaderGeneratorInterface, u8> s_gs_disk_cache;
+static LinearDiskCache<ShaderGeneratorInterface, u8> s_ps_disk_cache;
+static LinearDiskCache<ShaderGeneratorInterface, u8> s_vs_disk_cache;
 
-UidChecker<GeometryShaderUid, ShaderCode> geometry_uid_checker;
-UidChecker<PixelShaderUid, ShaderCode> pixel_uid_checker;
-UidChecker<VertexShaderUid, ShaderCode> vertex_uid_checker;
+static UidChecker<GeometryShaderUid, ShaderCode> s_geometry_uid_checker;
+static UidChecker<PixelShaderUid, ShaderCode> s_pixel_uid_checker;
+static UidChecker<VertexShaderUid, ShaderCode> s_vertex_uid_checker;
 
-D3D12_SHADER_BYTECODE last_geometry_shader_bytecode;
-D3D12_SHADER_BYTECODE last_pixel_shader_bytecode;
-D3D12_SHADER_BYTECODE last_vertex_shader_bytecode;
-GeometryShaderUid last_geometry_shader_uid;
-PixelShaderUid last_pixel_shader_uid;
-VertexShaderUid last_vertex_shader_uid;
+static D3D12_SHADER_BYTECODE s_last_geometry_shader_bytecode;
+static D3D12_SHADER_BYTECODE s_last_pixel_shader_bytecode;
+static D3D12_SHADER_BYTECODE s_last_vertex_shader_bytecode;
+static GeometryShaderUid s_last_geometry_shader_uid;
+static PixelShaderUid s_last_pixel_shader_uid;
+static VertexShaderUid s_last_vertex_shader_uid;
 
 template<SHADER_STAGE stage>
-class ShaderCacheInserter : public LinearDiskCacheReader<ShaderGeneratorInterface, u8>
+class ShaderCacheInserter final : public LinearDiskCacheReader<ShaderGeneratorInterface, u8>
 {
 public:
 	void Read(const ShaderGeneratorInterface &key, const u8* value, u32 value_size)
@@ -66,12 +66,12 @@ void ShaderCache::Init()
 	// This class intentionally shares its shader cache files with DX11, as the shaders are (right now) identical.
 	// Reduces unnecessary compilation when switching between APIs.
 		
-	last_geometry_shader_bytecode = {};
-	last_pixel_shader_bytecode = {};
-	last_vertex_shader_bytecode = {};
-	last_geometry_shader_uid = {};
-	last_pixel_shader_uid = {};
-	last_vertex_shader_uid = {};
+	s_last_geometry_shader_bytecode = {};
+	s_last_pixel_shader_bytecode = {};
+	s_last_vertex_shader_bytecode = {};
+	s_last_geometry_shader_uid = {};
+	s_last_pixel_shader_uid = {};
+	s_last_vertex_shader_uid = {};
 
 	SETSTAT(stats.numPixelShadersAlive, 0);
 	SETSTAT(stats.numPixelShadersCreated, 0);
@@ -91,13 +91,13 @@ void ShaderCache::Init()
 	std::string vs_cache_filename = StringFromFormat("%sdx11-%s-vs.cache", shader_cache_path.c_str(), title_unique_id.c_str());
 
 	ShaderCacheInserter<SHADER_STAGE_GEOMETRY_SHADER> gs_inserter;
-	g_gs_disk_cache.OpenAndRead(gs_cache_filename, gs_inserter);
+	s_gs_disk_cache.OpenAndRead(gs_cache_filename, gs_inserter);
 
 	ShaderCacheInserter<SHADER_STAGE_PIXEL_SHADER> ps_inserter;
-	g_ps_disk_cache.OpenAndRead(ps_cache_filename, ps_inserter);
+	s_ps_disk_cache.OpenAndRead(ps_cache_filename, ps_inserter);
 
 	ShaderCacheInserter<SHADER_STAGE_VERTEX_SHADER> vs_inserter;
-	g_vs_disk_cache.OpenAndRead(vs_cache_filename, vs_inserter);
+	s_vs_disk_cache.OpenAndRead(vs_cache_filename, vs_inserter);
 
 	// Clear out disk cache when debugging shaders to ensure stale ones don't stick around..
 	if (g_Config.bEnableShaderDebugging)
@@ -111,36 +111,36 @@ void ShaderCache::Clear()
 
 void ShaderCache::Shutdown()
 {
-	for (auto& iter : gs_bytecode_cache)
+	for (auto& iter : s_gs_bytecode_cache)
 		SAFE_DELETE(iter.second.pShaderBytecode);
 
-	for (auto& iter : ps_bytecode_cache)
+	for (auto& iter : s_ps_bytecode_cache)
 		SAFE_DELETE(iter.second.pShaderBytecode);
 
-	for (auto& iter : vs_bytecode_cache)
+	for (auto& iter : s_vs_bytecode_cache)
 		SAFE_DELETE(iter.second.pShaderBytecode);
 
-	gs_bytecode_cache.clear();
-	ps_bytecode_cache.clear();
-	vs_bytecode_cache.clear();
+	s_gs_bytecode_cache.clear();
+	s_ps_bytecode_cache.clear();
+	s_vs_bytecode_cache.clear();
 
-	g_gs_disk_cache.Sync();
-	g_gs_disk_cache.Close();
-	g_ps_disk_cache.Sync();
-	g_ps_disk_cache.Close();
-	g_vs_disk_cache.Sync();
-	g_vs_disk_cache.Close();
+	s_gs_disk_cache.Sync();
+	s_gs_disk_cache.Close();
+	s_ps_disk_cache.Sync();
+	s_ps_disk_cache.Close();
+	s_vs_disk_cache.Sync();
+	s_vs_disk_cache.Close();
 
 	if (g_Config.bEnableShaderDebugging)
 	{
-		gs_hlsl_cache.clear();
-		ps_hlsl_cache.clear();
-		vs_hlsl_cache.clear();
+		s_gs_hlsl_cache.clear();
+		s_ps_hlsl_cache.clear();
+		s_vs_hlsl_cache.clear();
 	}
 
-	geometry_uid_checker.Invalidate();
-	pixel_uid_checker.Invalidate();
-	vertex_uid_checker.Invalidate();
+	s_geometry_uid_checker.Invalidate();
+	s_pixel_uid_checker.Invalidate();
+	s_vertex_uid_checker.Invalidate();
 }
 
 void ShaderCache::LoadAndSetActiveShaders(DSTALPHA_MODE ps_dst_alpha_mode, u32 gs_primitive_type)
@@ -148,13 +148,13 @@ void ShaderCache::LoadAndSetActiveShaders(DSTALPHA_MODE ps_dst_alpha_mode, u32 g
 	switch (gs_primitive_type)
 	{
 	case PRIMITIVE_TRIANGLES:
-		current_primitive_topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+		s_current_primitive_topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 		break;
 	case PRIMITIVE_LINES:
-		current_primitive_topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_LINE;
+		s_current_primitive_topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_LINE;
 		break;
 	case PRIMITIVE_POINTS:
-		current_primitive_topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_POINT;
+		s_current_primitive_topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_POINT;
 		break;
 	default:
 		CHECK(0, "Invalid primitive type.");
@@ -165,9 +165,9 @@ void ShaderCache::LoadAndSetActiveShaders(DSTALPHA_MODE ps_dst_alpha_mode, u32 g
 	PixelShaderUid ps_uid = GetPixelShaderUid(ps_dst_alpha_mode, API_D3D);
 	VertexShaderUid vs_uid = GetVertexShaderUid(API_D3D);
 
-	bool gs_changed = gs_uid != last_geometry_shader_uid;
-	bool ps_changed = ps_uid != last_pixel_shader_uid;
-	bool vs_changed = vs_uid != last_vertex_shader_uid;
+	bool gs_changed = gs_uid != s_last_geometry_shader_uid;
+	bool ps_changed = ps_uid != s_last_pixel_shader_uid;
+	bool vs_changed = vs_uid != s_last_vertex_shader_uid;
 
 	if (!gs_changed && !ps_changed && !vs_changed)
 	{
@@ -183,19 +183,19 @@ void ShaderCache::LoadAndSetActiveShaders(DSTALPHA_MODE ps_dst_alpha_mode, u32 g
 		if (gs_changed)
 		{
 			code = GenerateGeometryShaderCode(gs_primitive_type, API_D3D);
-			geometry_uid_checker.AddToIndexAndCheck(code, gs_uid, "Geometry", "g");
+			s_geometry_uid_checker.AddToIndexAndCheck(code, gs_uid, "Geometry", "g");
 		}
 
 		if (ps_changed)
 		{
 			code = GeneratePixelShaderCode(ps_dst_alpha_mode, API_D3D);
-			pixel_uid_checker.AddToIndexAndCheck(code, ps_uid, "Pixel", "p");
+			s_pixel_uid_checker.AddToIndexAndCheck(code, ps_uid, "Pixel", "p");
 		}
 		
 		if (vs_changed)
 		{
 			code = GenerateVertexShaderCode(API_D3D);
-			vertex_uid_checker.AddToIndexAndCheck(code, vs_uid, "Vertex", "v");
+			s_vertex_uid_checker.AddToIndexAndCheck(code, vs_uid, "Vertex", "v");
 		}
 	}
 
@@ -203,14 +203,14 @@ void ShaderCache::LoadAndSetActiveShaders(DSTALPHA_MODE ps_dst_alpha_mode, u32 g
 	{
 		if (gs_uid.GetUidData()->IsPassthrough())
 		{
-			last_geometry_shader_bytecode = {};
+			s_last_geometry_shader_bytecode = {};
 		}
 		else
 		{
-			auto gs_iterator = gs_bytecode_cache.find(gs_uid);
-			if (gs_iterator != gs_bytecode_cache.end())
+			auto gs_iterator = s_gs_bytecode_cache.find(gs_uid);
+			if (gs_iterator != s_gs_bytecode_cache.end())
 			{
-				last_geometry_shader_bytecode = gs_iterator->second;
+				s_last_geometry_shader_bytecode = gs_iterator->second;
 			}
 			else
 			{
@@ -227,7 +227,7 @@ void ShaderCache::LoadAndSetActiveShaders(DSTALPHA_MODE ps_dst_alpha_mode, u32 g
 
 				if (g_ActiveConfig.bEnableShaderDebugging && gs_bytecode)
 				{
-					gs_hlsl_cache[gs_uid] = gs_code.GetBuffer();
+					s_gs_hlsl_cache[gs_uid] = gs_code.GetBuffer();
 				}
 			}
 		}
@@ -235,10 +235,10 @@ void ShaderCache::LoadAndSetActiveShaders(DSTALPHA_MODE ps_dst_alpha_mode, u32 g
 
 	if (ps_changed)
 	{
-		auto ps_iterator = ps_bytecode_cache.find(ps_uid);
-		if (ps_iterator != ps_bytecode_cache.end())
+		auto ps_iterator = s_ps_bytecode_cache.find(ps_uid);
+		if (ps_iterator != s_ps_bytecode_cache.end())
 		{
-			last_pixel_shader_bytecode = ps_iterator->second;
+			s_last_pixel_shader_bytecode = ps_iterator->second;
 			GFX_DEBUGGER_PAUSE_AT(NEXT_PIXEL_SHADER_CHANGE, true);
 		}
 		else
@@ -256,7 +256,7 @@ void ShaderCache::LoadAndSetActiveShaders(DSTALPHA_MODE ps_dst_alpha_mode, u32 g
 
 			if (g_ActiveConfig.bEnableShaderDebugging && ps_bytecode)
 			{
-				ps_hlsl_cache[ps_uid] = ps_code.GetBuffer();
+				s_ps_hlsl_cache[ps_uid] = ps_code.GetBuffer();
 			}
 
 			ps_bytecode->Release();
@@ -265,10 +265,10 @@ void ShaderCache::LoadAndSetActiveShaders(DSTALPHA_MODE ps_dst_alpha_mode, u32 g
 
 	if (vs_changed)
 	{
-		auto vs_iterator = vs_bytecode_cache.find(vs_uid);
-		if (vs_iterator != vs_bytecode_cache.end())
+		auto vs_iterator = s_vs_bytecode_cache.find(vs_uid);
+		if (vs_iterator != s_vs_bytecode_cache.end())
 		{
-			last_vertex_shader_bytecode = vs_iterator->second;
+			s_last_vertex_shader_bytecode = vs_iterator->second;
 			GFX_DEBUGGER_PAUSE_AT(NEXT_PIXEL_SHADER_CHANGE, true);
 		}
 		else
@@ -286,7 +286,7 @@ void ShaderCache::LoadAndSetActiveShaders(DSTALPHA_MODE ps_dst_alpha_mode, u32 g
 
 			if (g_ActiveConfig.bEnableShaderDebugging && vs_bytecode)
 			{
-				vs_hlsl_cache[vs_uid] = vs_code.GetBuffer();
+				s_vs_hlsl_cache[vs_uid] = vs_code.GetBuffer();
 			}
 		}
 	}
@@ -307,23 +307,23 @@ void ShaderCache::InsertByteCode(const ShaderGeneratorInterface& uid, SHADER_STA
 	switch (stage)
 	{
 	case SHADER_STAGE_GEOMETRY_SHADER:
-		gs_bytecode_cache[reinterpret_cast<const GeometryShaderUid&>(uid)] = shader_bytecode;
-		last_geometry_shader_bytecode = shader_bytecode;
-		g_gs_disk_cache.Append(uid, bytecode_blob->Data(), bytecode_blob->Size());
+		s_gs_bytecode_cache[reinterpret_cast<const GeometryShaderUid&>(uid)] = shader_bytecode;
+		s_last_geometry_shader_bytecode = shader_bytecode;
+		s_gs_disk_cache.Append(uid, bytecode_blob->Data(), bytecode_blob->Size());
 		break;
 	case SHADER_STAGE_PIXEL_SHADER:
-		ps_bytecode_cache[reinterpret_cast<const PixelShaderUid&>(uid)] = shader_bytecode;
-		last_pixel_shader_bytecode = shader_bytecode;
-		g_ps_disk_cache.Append(uid, bytecode_blob->Data(), bytecode_blob->Size());
+		s_ps_bytecode_cache[reinterpret_cast<const PixelShaderUid&>(uid)] = shader_bytecode;
+		s_last_pixel_shader_bytecode = shader_bytecode;
+		s_ps_disk_cache.Append(uid, bytecode_blob->Data(), bytecode_blob->Size());
 		INCSTAT(stats.numPixelShadersCreated);
-		SETSTAT(stats.numPixelShadersAlive, (int)ps_bytecode_cache.size());
+		SETSTAT(stats.numPixelShadersAlive, (int)s_ps_bytecode_cache.size());
 		break;
 	case SHADER_STAGE_VERTEX_SHADER:
-		vs_bytecode_cache[reinterpret_cast<const VertexShaderUid&>(uid)] = shader_bytecode;
-		last_vertex_shader_bytecode = shader_bytecode;
-		g_vs_disk_cache.Append(uid, bytecode_blob->Data(), bytecode_blob->Size());
+		s_vs_bytecode_cache[reinterpret_cast<const VertexShaderUid&>(uid)] = shader_bytecode;
+		s_last_vertex_shader_bytecode = shader_bytecode;
+		s_vs_disk_cache.Append(uid, bytecode_blob->Data(), bytecode_blob->Size());
 		INCSTAT(stats.numVertexShadersCreated);
-		SETSTAT(stats.numVertexShadersAlive, (int)vs_bytecode_cache.size());
+		SETSTAT(stats.numVertexShadersAlive, (int)s_vs_bytecode_cache.size());
 		break;
 	default:
 		CHECK(0, "Invalid shader stage specified.");
@@ -337,11 +337,11 @@ D3D12_SHADER_BYTECODE ShaderCache::GetActiveShaderBytecode(SHADER_STAGE stage)
 	switch (stage)
 	{
 	case SHADER_STAGE_GEOMETRY_SHADER:
-		return last_geometry_shader_bytecode;
+		return s_last_geometry_shader_bytecode;
 	case SHADER_STAGE_PIXEL_SHADER:
-		return last_pixel_shader_bytecode;
+		return s_last_pixel_shader_bytecode;
 	case SHADER_STAGE_VERTEX_SHADER:
-		return last_vertex_shader_bytecode;
+		return s_last_vertex_shader_bytecode;
 	default:
 		CHECK(0, "Invalid shader stage specified.");
 		return D3D12_SHADER_BYTECODE();
@@ -350,7 +350,7 @@ D3D12_SHADER_BYTECODE ShaderCache::GetActiveShaderBytecode(SHADER_STAGE stage)
 
 D3D12_PRIMITIVE_TOPOLOGY_TYPE ShaderCache::GetCurrentPrimitiveTopology()
 {
-	return current_primitive_topology;
+	return s_current_primitive_topology;
 }
 
 const ShaderGeneratorInterface* ShaderCache::GetActiveShaderUid(SHADER_STAGE stage)
@@ -358,11 +358,11 @@ const ShaderGeneratorInterface* ShaderCache::GetActiveShaderUid(SHADER_STAGE sta
 	switch (stage)
 	{
 	case SHADER_STAGE_GEOMETRY_SHADER:
-		return &last_geometry_shader_uid;
+		return &s_last_geometry_shader_uid;
 	case SHADER_STAGE_PIXEL_SHADER:
-		return &last_pixel_shader_uid;
+		return &s_last_pixel_shader_uid;
 	case SHADER_STAGE_VERTEX_SHADER:
-		return &last_vertex_shader_uid;
+		return &s_last_vertex_shader_uid;
 	default:
 		CHECK(0, "Invalid shader stage specified.");
 		return nullptr;
@@ -374,11 +374,11 @@ D3D12_SHADER_BYTECODE ShaderCache::GetShaderFromUid(SHADER_STAGE stage, const Sh
 	switch (stage)
 	{
 	case SHADER_STAGE_GEOMETRY_SHADER:
-		return gs_bytecode_cache[*reinterpret_cast<const GeometryShaderUid*>(uid)];
+		return s_gs_bytecode_cache[*reinterpret_cast<const GeometryShaderUid*>(uid)];
 	case SHADER_STAGE_PIXEL_SHADER:
-		return ps_bytecode_cache[*reinterpret_cast<const PixelShaderUid*>(uid)];
+		return s_ps_bytecode_cache[*reinterpret_cast<const PixelShaderUid*>(uid)];
 	case SHADER_STAGE_VERTEX_SHADER:
-		return vs_bytecode_cache[*reinterpret_cast<const VertexShaderUid*>(uid)];
+		return s_vs_bytecode_cache[*reinterpret_cast<const VertexShaderUid*>(uid)];
 	default:
 		CHECK(0, "Invalid shader stage specified.");
 		return D3D12_SHADER_BYTECODE();
